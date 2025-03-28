96588c267d42806f5a8111020d68fea8bcb59b9d
Urvashi Mahavar <mahavurvashi@gmail.com>
2025-03-28 15:26:29 +0530

    10-March

---
+++ src/JavaBasic/Println_vs_Print.java
@@ -0,0 +1,11 @@
+package JavaBasic;
+
+public class Println_vs_Print {
+    public static void main(String[] args) {
+//        System.out.println("Hello World"); This will print in new line, ln stands for new line
+//        System.out.println("Hello World2");
+
+        System.out.print("Hello World3"); // This will print in same line
+        System.out.print("Hello World4");
+    }
+}
